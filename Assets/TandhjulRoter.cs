463cb918f090b7db6abcceb62789340e56db2a2e
Malou86 <marielouisechristensen@Marie-Louise-Christensens-MacBook-Pro.local>
2015-02-08 00:54:00 +0100

    samlebaand

---
+++ Assets/TandhjulRoter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TandhjulRoter : MonoBehaviour {
+
+	[SerializeField] private float retning;
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		transform.Rotate (0f, 1f * retning, 0f);	
+	}
+}

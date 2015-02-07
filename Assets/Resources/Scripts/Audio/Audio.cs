a86f8aeab687ef4cc50482abf55e148f6b62a27e
lukaswendt <lukwendt@gmail.com>
2015-02-07 12:36:18 +0100

    Added simple audio implementation

---
+++ Assets/Resources/Scripts/Audio/Audio.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Audio {
+
+	public static AudioManager audioManager;
+
+	public static void Play(SoundEvent soundEvent)
+	{
+		audioManager.Play(soundEvent);
+	}
+
+}
---
+++ Assets/Resources/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AudioManager : MonoBehaviour {
+
+	private Dictionary<SoundEvent, AudioComponent> audioComponents = new Dictionary<SoundEvent, AudioComponent>();
+
+	[SerializeField] private AudioComponent jump; 
+
+	void Awake()
+	{
+		Audio.audioManager = this;
+
+		audioComponents.Add(SoundEvent.Jump, jump);
+	}
+
+	public void Play(SoundEvent soundEvent)
+	{
+		audioComponents[soundEvent].Play();
+	}
+}
+
+public enum SoundEvent
+{
+	Jump,
+	Reward
+}

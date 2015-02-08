a36fc44928f2eb61fb2d3b4d8e7cb85d8db7e511
Thomas Malowanczyk <s103407@student.dtu.dk>
2015-02-08 01:39:21 +0100

    Made a stretch script for the player

---
+++ Assets/Resources/Scripts/VelocityStretch.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VelocityStretch : MonoBehaviour {
+
+    public float effect;
+    public Transform player;
+    private Vector3 originScale;
+
+	// Use this for initialization
+	void Start () {
+        originScale = player.localScale;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        //Debug.Log(player.rigidbody.velocity);
+        
+        float veloc = player.rigidbody.velocity.y*effect;
+        Debug.Log(veloc);
+        player.localScale = new Vector3(originScale.x - originScale.x * veloc,originScale.y + originScale.y*veloc,originScale.z - originScale.z*veloc);
+        
+	}
+}

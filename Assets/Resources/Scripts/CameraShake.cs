015f8e0424b28a3244e188dd819e5207bc6966e8
Thomas Malowanczyk <s103407@student.dtu.dk>
2015-02-08 11:12:30 +0100

    Added shake and fixed jump along with band with

---
+++ Assets/Resources/Scripts/CameraShake.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour
+{
+    Quaternion originRotation;
+
+    public float intensity;
+    public float decay;
+
+    float shake_decay;
+    float shake_intensity;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            StartShake();
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            StopShake();
+        }
+
+         if(shake_intensity > 0)
+        {
+            transform.rotation = new Quaternion(
+                            originRotation.x + Random.Range(-shake_intensity, shake_intensity) * .2f,
+                            originRotation.y + Random.Range(-shake_intensity, shake_intensity) * .2f,
+                            originRotation.z + Random.Range(-shake_intensity, shake_intensity) * .2f,
+                            originRotation.w + Random.Range(-shake_intensity, shake_intensity) * .2f);
+        }
+    }
+
+    public void StartShake()
+    {
+        originRotation = transform.rotation;
+        shake_intensity = intensity;
+    }
+
+    public void StopShake()
+    {
+        shake_intensity = 0;
+        transform.rotation = originRotation;
+    }
+
+    public void GameOver()
+    {
+        StopShake();
+    }
+
+    public void Swap()
+    {
+        StopShake();
+    }
+}
